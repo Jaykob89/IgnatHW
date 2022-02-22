9707bda69f52aaf37cd65f5e64202e305c0a2b95
ivanin_j <ivanin.jay@gmail.com>
2022-02-22 23:19:51 +0200

    HW10

---
+++ src/p2-homeworks/h10/Preloader.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import preloader from './image/b4d657e7ef262b88eb5f7ac021edda87.gif'
+
+export const Preloader = () => {
+    return <img src={preloader} height={'30px'} width={'30px'}/>
+}
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,26 @@
 const initState = {
-
+    isLoading: false
+}
+type initialStateType = {
+    isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: actionType): initialStateType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case "CHANGE-LOADING": {
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isLoading: boolean): actionType => ({type: "CHANGE-LOADING", isLoading} as const) // fix any
+
+type actionType = {
+    type: string,
+    isLoading: boolean
+}
